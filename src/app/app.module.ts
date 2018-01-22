d5ea7c10c24c58ae710afde22cbc92ab876e4224
Iarlen <iarlensilva775@gmail.com>
2018-01-21 23:02:35 -0300

    Configurando o provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
 
 
 @NgModule({
@@ -22,7 +23,9 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
     BrowserModule,
     routing
   ],
-  providers: [],
+  providers: [
+    CursosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
