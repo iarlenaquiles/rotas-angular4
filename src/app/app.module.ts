169197527583610e2130113b9015e615f235a1b2
Iarlen <iarlensilva775@gmail.com>
2018-01-21 21:34:59 -0300

    Criando o componente cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
