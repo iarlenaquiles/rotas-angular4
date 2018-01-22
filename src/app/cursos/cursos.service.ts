5b01e167c88ef1cf61f29e805f3f44a352d19743
Iarlen <iarlensilva775@gmail.com>
2018-01-21 23:08:35 -0300

    Criando o getCursos

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService {
 
+  getCursos() {
+    return [
+      {id: 1, nome: 'Angular 5'},
+      {id: 2, nome: 'Java'}
+    ];
+  }
+
   constructor() { }
 
 }
