68049041c116049afc2e8847abadc8a345e066ff
Iarlen <iarlensilva775@gmail.com>
2018-01-21 22:21:23 -0300

    Configurando o curso detalhe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { CursosComponent } from './cursos/cursos.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 const APP_ROUTES: Routes = [
@@ -15,6 +16,9 @@ const APP_ROUTES: Routes = [
   },
   {
     path: 'cursos', component: CursosComponent
+  },
+  {
+    path: 'cursos/:id', component: CursoDetalheComponent
   }
 ];
 
