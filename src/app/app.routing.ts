bf63e08d4adf84708a4d5171ea76e046d6819cf9
Iarlen <iarlensilva775@gmail.com>
2018-01-21 21:50:24 -0300

    Criando as rotas básicas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 const APP_ROUTES: Routes = [
-
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'cursos', component: CursosComponent
+  }
 ];
