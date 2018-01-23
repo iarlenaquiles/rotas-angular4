41c4b7767ba7730243a61a7777a9eb31671e2f19
Iarlen <iarlensilva775@gmail.com>
2018-01-22 23:44:04 -0300

    Importando o routermodule no modulo de cursos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -8,7 +9,8 @@ import { CursosService } from './cursos.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     CursosComponent,
