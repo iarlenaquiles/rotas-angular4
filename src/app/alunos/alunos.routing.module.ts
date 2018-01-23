7149dbad8bdbb2674d938d4a861f28fbc3d5cc02
Iarlen <iarlensilva775@gmail.com>
2018-01-23 00:17:47 -0300

    Criando as rotas do aluno

---
+++ src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,27 @@
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+
+const alunosRoutes: Routes = [
+  {
+    path: 'alunos', component: AlunosComponent
+  },
+  {
+    path: 'alunos/:id', component: AlunoDetalheComponent
+  },
+  {
+    path: 'alunos/:id/editar', component: AlunoFormComponent
+  },
+  {
+    path: 'alunos/novo', component: AlunoFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(alunosRoutes)],
+  exports: [RouterModule]
+})
+export class AlunosRoutingModule {}
