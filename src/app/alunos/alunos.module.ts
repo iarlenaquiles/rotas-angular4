a74f54caf027bda61a9335a986fb370fd4616e34
Iarlen <iarlensilva775@gmail.com>
2018-01-23 19:28:54 -0300

    Configurando o provider

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   imports: [
     CommonModule,
     AlunosRoutingModule
   ],
-  declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent]
+  declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
+  providers: [ AlunosService]
+
 })
 export class AlunosModule { }
