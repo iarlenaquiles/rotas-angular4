254770c9e453974a8412ac842d7f1cc0df2ae994
Iarlen <iarlensilva775@gmail.com>
2018-01-22 23:39:36 -0300

    Criando o módulo de funcionalidade

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursosComponent } from './cursos.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CursosComponent,
+    CursoNaoEncontradoComponent,
+    CursoDetalheComponent
+  ],
+  providers: [
+    CursosService
+  ]
+})
+export class CursosModule { }
