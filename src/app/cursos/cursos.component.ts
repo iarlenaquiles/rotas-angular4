58df9d399163ba7f7299e6a48fb469b16bec25e1
Iarlen <iarlensilva775@gmail.com>
2018-01-22 22:39:48 -0300

    Extraindo parâmetro da url

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CursosService } from './cursos.service';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-cursos',
@@ -14,11 +15,23 @@ export class CursosComponent implements OnInit {
 
   pagina: number;
 
+  inscricao: Subscription;
+
   constructor(private cursosService: CursosService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
