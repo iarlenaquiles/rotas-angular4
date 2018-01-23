32b2aaa2430f14684f5ed44812809cea58c49fe9
Iarlen <iarlensilva775@gmail.com>
2018-01-23 00:10:40 -0300

    Criando o form do aluno

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { AlunosComponent } from './alunos.component';
 import { NgModule } from '@angular/core';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AlunosComponent]
+  declarations: [AlunosComponent, AlunoFormComponent]
 })
 export class AlunosModule { }
