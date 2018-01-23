ba9c8d73d5205948d8fbfa1b0b3b32903b33b26a
Iarlen <iarlensilva775@gmail.com>
2018-01-22 22:59:47 -0300

    Criando o módulo de rotas e exportando o módulo

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,4 +1,34 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
-@NgModule
+
+const appRoutes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'cursos', component: CursosComponent
+  },
+  {
+    path: 'curso/:id', component: CursoDetalheComponent
+  },
+  {
+    path: 'naoEncontrado', component: CursoNaoEncontradoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
 export class AppRoutingModule {}
