f42b964e0e0068f1a66b8fada0860a35c8ef9bba
Iarlen <iarlensilva775@gmail.com>
2018-01-23 19:32:26 -0300

    criando um array de alunos

--- src/app/alunos/alunos.service.ts
+++ src/app/alunos/alunos.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AlunosService {
 
+  private alunos: any[] = [
+    {id: 1, nome: 'Aluno 01', email: 'aluno1@email.com'},
+    {id: 2, nome: 'Aluno 02', email: 'aluno2@email.com'},
+    {id: 3, nome: 'Aluno 03', email: 'aluno3@email.com'}
+  ];
+
   constructor() { }
 
 }
