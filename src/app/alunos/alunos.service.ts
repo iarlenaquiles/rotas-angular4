8a296ef9c2a67d38a430c6cd92d2ab2edf6b1242
Iarlen <iarlensilva775@gmail.com>
2018-01-23 20:58:55 -0300

    Tipando o array

--- src/app/alunos/alunos.service.ts
+++ src/app/alunos/alunos.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Aluno } from './aluno';
 
 @Injectable()
 export class AlunosService {
 
-  private alunos: any[] = [
+  private alunos: Aluno[] = [
     {id: 1, nome: 'Aluno 01', email: 'aluno1@email.com'},
     {id: 2, nome: 'Aluno 02', email: 'aluno2@email.com'},
     {id: 3, nome: 'Aluno 03', email: 'aluno3@email.com'}
