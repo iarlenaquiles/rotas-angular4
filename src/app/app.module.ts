80226fbce93e52ac91015271ab079abdc18f8a28
Iarlen <iarlensilva775@gmail.com>
2018-01-22 23:43:35 -0300

    Movendo para o modulo de funcionalidade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     //routing
-    AppRoutingModule
+    CursosModule,
+    AppRoutingModule,
   ],
   providers: [
-    CursosService
   ],
   bootstrap: [AppComponent]
 })
