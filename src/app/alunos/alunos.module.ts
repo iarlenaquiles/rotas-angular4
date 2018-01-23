c319a97247bf6e75a7524a227437e6b4f09901e8
Iarlen <iarlensilva775@gmail.com>
2018-01-23 00:11:43 -0300

    Criando o componente de detalhe do aluno

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { AlunosComponent } from './alunos.component';
 import { NgModule } from '@angular/core';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AlunosComponent, AlunoFormComponent]
+  declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent]
 })
 export class AlunosModule { }
