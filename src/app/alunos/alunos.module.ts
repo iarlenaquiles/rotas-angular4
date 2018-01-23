d04c2675ae69d609d2787c54440c92d1f01b0d76
Iarlen <iarlensilva775@gmail.com>
2018-01-23 00:09:22 -0300

    Criando modulo de alunos

---
+++ src/app/alunos/alunos.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { AlunosComponent } from './alunos.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AlunosComponent]
+})
+export class AlunosModule { }
