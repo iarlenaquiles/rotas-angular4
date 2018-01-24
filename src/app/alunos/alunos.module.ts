41526233011f5343b32f37f2fdae15d51fed2bf4
Iarlen <iarlensilva775@gmail.com>
2018-01-23 21:13:54 -0300

    Configurando o forms module

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -5,11 +5,13 @@ import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
 import { AlunosService } from './alunos.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AlunosRoutingModule
+    AlunosRoutingModule,
+    FormsModule
   ],
   declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
   providers: [ AlunosService]
