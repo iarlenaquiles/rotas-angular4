adf34cbef9954b1d7eb3a2cdb5ccaf1aa982dc2e
Iarlen <iarlensilva775@gmail.com>
2018-02-03 13:09:26 -0300

    Removendo a importação do cursosmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
+//import { CursosModule } from './cursos/cursos.module';
 import { AlunosModule } from './alunos/alunos.module';
 
 
@@ -21,7 +21,7 @@ import { AlunosModule } from './alunos/alunos.module';
   imports: [
     BrowserModule,
     //routing
-    CursosModule,
+    //CursosModule,
     AppRoutingModule,
     AlunosModule
   ],
